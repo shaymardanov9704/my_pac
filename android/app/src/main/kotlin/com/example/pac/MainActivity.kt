c86c2e2d7c9dbf07ef546bb8a5d0964646f17782
Eldor Shoymardonov <shaymardanov9704@gmail.com>
2022-07-16 10:17:30 +0300

    Commit

---
+++ android/app/src/main/kotlin/com/example/pac/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pac
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ my_pac/test/my_pac_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:my_pac/my_pac.dart';
+
+void main() {
+  test('adds one to input values', () {
+  });
+}
